04290fbf269a546e67788d6371b6ff45afb7e4a6
Oscar Otero <oom@oscarotero.com>
2021-03-21 23:10:21 +0100

    removed cjs dependencies

--- deps.js
+++ deps.js
@@ -1,14 +1,7 @@
 export const EOL = Deno.build.os == "windows" ? "\r\n" : "\n";
 export { default as stable } from "https://jspm.dev/stable@0.1.8";
 export { default as specificity } from "./deps/csso-specificity.js";
+export { default as csso } from "./deps/csso.js";
+export { default as csstree } from "./deps/csstree.js";
 export * from "./deps/sax.js";
 export * from "https://jspm.dev/css-select@3.1.2";
-
-//CommonJS dependencies
-import { createRequire } from "https://deno.land/std@0.90.0/node/module.ts";
-
-const require = createRequire(import.meta.url);
-const csstree = require("./deps/csstree.js");
-const csso = require("./deps/csso.js");
-
-export { csso, csstree };
