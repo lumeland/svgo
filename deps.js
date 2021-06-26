ffeaa8daa0eb5ab8ab5b9499caf2465e0092742d
Valtteri Laitinen <dev@valtlai.fi>
2021-06-26 18:41:57 +0300

    [svgo 2.3.1] Upgrade css-select from `^3.1.2` to `^4.1.3`
    From svg/svgo@25d0f87e3321684f5529831c6b7b105a25f92f4b

--- deps.js
+++ deps.js
@@ -4,4 +4,4 @@ export { default as specificity } from "./deps/csso-specificity.js";
 export { default as csso } from "./deps/csso.js";
 export { default as csstree } from "./deps/csstree.js";
 export * from "./deps/sax.js";
-export * from "https://jspm.dev/css-select@3.1.2";
+export * from "https://jspm.dev/css-select@4.1.3";
